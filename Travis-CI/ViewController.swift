2f88f2dca60c7704f06b977deaa6c5060d173236
Danh Hung <frrzzy@gmail.com>
2018-04-13 00:49:08 +0700

    Demo 3

--- Travis-CI/ViewController.swift
+++ Travis-CI/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello world 2")
+        print("Hello world 3")
     }
 
     override func didReceiveMemoryWarning() {
